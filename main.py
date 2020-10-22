420be300dfd2bda5eb0e54933440b47469d49c4e
Krackasourus <krackasourus@aim.com>
2020-10-22 16:57:40 -0500

    Added logo.gif Changed the theme to black Removed the top frame Added Custom top frame Added logic to place the window in the middle of the screen (needs more work)

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
-from tkinter import ttk
+from tkinter import ttk, PhotoImage
 import ttkthemes
 
 
 def main():
-    root = ttkthemes.ThemedTk(theme='equilux')
-    root.title("Hello World")
 
-    labelframe = ttk.LabelFrame(root)
+    def move_window(event):
+        root.geometry(f'+{event.x_root}+{event.y_root}')
+
+    root = ttkthemes.ThemedTk(theme='black')
+    root.overrideredirect(True)  # removes the window bar
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    size = tuple(int(_) for _ in root.geometry().split('+')[0].split('x'))
+    x = (screen_width / 2 - size[0] / 2)
+    y = (screen_height / 2 - size[1] / 2)
+    root.geometry("+%d+%d" % (x, y))
+
+    core_frame = ttk.Frame(root, relief='ridge', border=1)
+    core_frame.pack()
+
+    handle_frame = ttk.Frame(core_frame, relief='raised')  # recreates the top bar
+    handle_frame.pack(fill='x', expand=True)
+
+    app_logo = PhotoImage(file='logo.gif')
+    app_logo = app_logo.zoom(8)  # resize the image
+    app_logo = app_logo.subsample(32)  # resample the image
+
+    handle_frame_logo = ttk.Label(handle_frame, image=app_logo, relief='raised')
+    handle_frame_logo.image = app_logo
+    handle_frame_logo.pack(side='left', padx=2)
+
+    app_title = ttk.Label(handle_frame, text=" App Title   ")
+    app_title.pack(side='left', padx=2)
+
+    close_button = ttk.Button(handle_frame, text='X', command=root.destroy)  # creates a button for the top bar to exit
+    close_button.pack(side='right')
+
+    labelframe = ttk.Frame(core_frame, relief='raised')
     labelframe.pack()
 
     greeting = ttk.Label(labelframe, text="Hello, World!")
-    greeting.grid(row=0, column=0, columnspan=3)
+    greeting.grid(row=0, column=0, columnspan=3, pady=5)
 
     prompt = ttk.Label(labelframe, text="Enter your name:")
-    prompt.grid(row=1, column=0)
+    prompt.grid(row=1, column=0, padx=(5, 0))
 
-    entry = ttk.Entry(labelframe, width=25)
+    entry = ttk.Entry(labelframe, width=25, justify='c')
     entry.grid(row=1, column=1)
 
-    button = ttk.Button(labelframe, text="Submit", command=lambda: greeting.config(text=f"Hello, {entry.get()}!"))
-    button.grid(row=1, column=2)
+    button = ttk.Button(labelframe, text="Submit", command=lambda: greeting.config(text=f"Hello, {entry.get().title()   }!"))
+    button.grid(row=1, column=2, padx=(0, 5))
+
+    handle_frame.bind('<B1-Motion>', move_window)
+    handle_frame_logo.bind('<B1-Motion>', move_window)
+    app_title.bind('<B1-Motion>', move_window)
 
     root.mainloop()
 
