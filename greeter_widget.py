0592f7b759ae1b58b362b874aed1a43351a30388
Christian Reall-Fluharty <creallfluharty@gmail.com>
2020-10-22 22:30:03 -0500

    add password entry

--- greeter_widget.py
+++ greeter_widget.py
@@ -10,29 +10,37 @@ class GreeterWidget:
         self.prompt = None
         self.entry = None
         self.button = None
+        self.password_prompt = None
+        self.password_entry = None
 
     def create(self):
         self.core_frame = ttk.Frame(self.parent, relief='ridge', border=1)
-        self.core_frame.pack()
+        self.core_frame.pack(ipadx=10, ipady=10)
 
         self.labelframe = ttk.Frame(self.core_frame, relief='raised')
         self.labelframe.pack()
 
         self.greeting = ttk.Label(self.labelframe, text="Hello, World!")
-        self.greeting.grid(row=0, column=0, columnspan=3, pady=5)
+        self.greeting.grid(row=0, column=0, columnspan=2, pady=5)
 
-        self.prompt = ttk.Label(self.labelframe, text="Enter your name:")
+        self.prompt = ttk.Label(self.labelframe, text="Enter your name:", justify='r')
         self.prompt.grid(row=1, column=0, padx=(5, 0))
 
-        self.entry = ttk.Entry(self.labelframe, width=25, justify='c')
+        self.entry = ttk.Entry(self.labelframe)
         self.entry.grid(row=1, column=1)
+
+        self.password_prompt = ttk.Label(self.labelframe, text="Enter your password:", justify='r')
+        self.password_prompt.grid(row=2, column=0)
+
+        self.password_entry = ttk.Entry(self.labelframe, show='*')
+        self.password_entry.grid(row=2, column=1)
 
         self.button = ttk.Button(
             self.labelframe,
             text="Submit",
             command=self.update_name,
         )
-        self.button.grid(row=1, column=2, padx=(0, 5))
+        self.button.grid(row=3, column=0, columnspan=2)
 
     def update_name(self):
         self.greeting.config(text=f"Hello, {self.entry.get().title()}!")
