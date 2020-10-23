1b293360e30a65ebef515c6518ca1d62f3ff5bbb
Krackasourus <krackasourus@aim.com>
2020-10-23 01:49:41 -0500

    REMOVED the top bar READDED the custom top bar     using platform module now able to detect the system platform and act accordingly ADDED a status bar thing ADDED stash fo no apparent reason ADJUSTED various widget padding to balance the window ADDED NEW IMPORT (platform) ADDED NEW IMPORT (datetime)

--- greeter_widget.py
+++ greeter_widget.py
@@ -14,33 +14,33 @@ class GreeterWidget:
         self.password_entry = None
 
     def create(self):
-        self.core_frame = ttk.Frame(self.parent, relief='ridge', border=1)
-        self.core_frame.pack(ipadx=10, ipady=10)
+        self.core_frame = ttk.Frame(self.parent, relief='sunken', border=1)
+        self.core_frame.pack(padx=10, pady=10)
 
         self.labelframe = ttk.Frame(self.core_frame, relief='raised')
-        self.labelframe.pack()
+        self.labelframe.pack(ipady=10)
 
         self.greeting = ttk.Label(self.labelframe, text="Hello, World!")
         self.greeting.grid(row=0, column=0, columnspan=2, pady=5)
 
-        self.prompt = ttk.Label(self.labelframe, text="Enter your name:", justify='r')
+        self.prompt = ttk.Label(self.labelframe, text="      Enter your name:", justify='r')
         self.prompt.grid(row=1, column=0, padx=(5, 0))
 
         self.entry = ttk.Entry(self.labelframe)
-        self.entry.grid(row=1, column=1)
+        self.entry.grid(row=1, column=1, padx=(0, 5))
 
         self.password_prompt = ttk.Label(self.labelframe, text="Enter your password:", justify='r')
-        self.password_prompt.grid(row=2, column=0)
+        self.password_prompt.grid(row=2, column=0, padx=(5, 0))
 
         self.password_entry = ttk.Entry(self.labelframe, show='*')
-        self.password_entry.grid(row=2, column=1)
+        self.password_entry.grid(row=2, column=1, padx=(0, 5))
 
         self.button = ttk.Button(
             self.labelframe,
             text="Submit",
             command=self.update_name,
         )
-        self.button.grid(row=3, column=0, columnspan=2)
+        self.button.grid(row=3, column=0, columnspan=2, pady=(10, 0))
 
     def update_name(self):
         self.greeting.config(text=f"Hello, {self.entry.get().title()}!")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from tkinter import ttk
+from tkinter import ttk, PhotoImage
 import ttkthemes
+import datetime
+import platform
 
 from greeter_widget import GreeterWidget
 
@@ -21,24 +23,74 @@ class Centerer:
 
 
 def main():
+    def move_window(event):
+        root.geometry(f'+{event.x_root}+{event.y_root}')
+
     root = ttkthemes.ThemedTk(theme='black')
     root.iconbitmap("./assets/img/icon_w_background.ico")
     root.title("Greeter")
+    root.lift()
+    root.attributes("-topmost", True)
+    # remove the top bar. Even on macs...
+    root.overrideredirect(True)
+    if platform.system() == 'Darwin':
+        root.overrideredirect(False)
+    elif platform.system() == 'Windows':
+        print('Windows detected +5 to bonus rolls')
 
     centerer = Centerer(root)
     centerer.set_callback()
 
-    tab_controller = ttk.Notebook(root)
+    master_container = ttk.Frame(root, relief='raised', border=5)
+    master_container.pack()
+    root_containter = ttk.Frame(master_container)
+    root_containter.pack(fill='x', expand=True)
+
+    handle_frame = ttk.Frame(root_containter, relief='raised')  # recreates the top bar
+    handle_frame.pack(fill='x', expand=True)
+
+    app_logo = PhotoImage(file='assets/img/logo.gif')
+    app_logo = app_logo.zoom(8)  # resize the image
+    app_logo = app_logo.subsample(32)  # resample the image
+
+    handle_frame_logo = ttk.Button(handle_frame,
+                                   image=app_logo,
+                                   command=lambda: print('File menu:')
+                                   )
+    handle_frame_logo.image = app_logo
+    handle_frame_logo.pack(side='left')
+
+    app_title = ttk.Label(handle_frame, text=" App Title   ")
+    app_title.pack(side='left', padx=2)
+
+    close_button = ttk.Button(handle_frame, text='X', command=root.destroy)  # creates a button for the top bar to exit
+    close_button.pack(side='right')
+
+    tab_controller = ttk.Notebook(root_containter)
     tab_controller.pack(expand=True, fill='both')
 
-    other_tab_label = ttk.Label(tab_controller)
+    other_tab_label = ttk.Label(tab_controller, text='Where be dis?', relief='sunken')
     other_tab_label.pack()
+
+    other_tab_label2 = ttk.Label(tab_controller, text='Where be dis?', relief='sunken')
+    other_tab_label2.pack()
 
     greeter_widget = GreeterWidget(tab_controller)
     greeter_widget.create()
 
     tab_controller.add(greeter_widget.get_frame(), text='main')
     tab_controller.add(other_tab_label, text='other')
+    tab_controller.add(other_tab_label2, text='stash')
+
+    status_bar = ttk.Frame(master_container, relief='sunken')
+    status_bar.pack(fill='both', expand=True)
+
+    date_label = ttk.Label(status_bar, text=str(datetime.datetime.today()).split(' ')[0])
+    date_label.pack(side='right', pady=2, padx=2)
+
+    handle_frame.bind('<B1-Motion>', move_window)
+    handle_frame_logo.bind('<B1-Motion>', move_window)
+    app_title.bind('<B1-Motion>', move_window)
 
     root.mainloop()
 
