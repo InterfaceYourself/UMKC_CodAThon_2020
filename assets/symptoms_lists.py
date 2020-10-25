76f946b43a27c1f3ea2dd178c267870a83def75a
sogent <sa.ogent95@gmail.com>
2020-10-25 16:34:47 -0500

    ADDED more symptoms to non-covid symptoms list

--- assets/symptoms_lists.py
+++ assets/symptoms_lists.py
@@ -29,11 +29,9 @@ non_covid_symptoms = [
     'Shortness of breath',
     'Difficulty breathing',
     'Fatigue',
-    'Muscle',
+    'Muscle cramps',
     'Body aches',
     'Headache',
-    'New loss of taste',
-    'New loss of smell',
     'Sore throat',
     'Congestion',
     'Runny nose',
@@ -49,7 +47,31 @@ non_covid_symptoms = [
     'Ringing in ears',
     'Nose bleeding',
     'Swollen gums',
-    'Irregular heartbeat'
+    'Irregular heartbeat',
+    'Sensitive to light',
+    'Drastic mood swings',
+    'Shaking or trembling hands',
+    'Puffy eyelids and swelling',
+    'Stomach cramps',
+    'Skin cracking and peeling',
+    'White patches on tongue',
+    'Cloudy vision',
+    'Restlessness',
+    'Vision loss',
+    'Knee swelling',
+    'Vomiting blood',
+    'Acute lower back pain',
+    'Burning sensation while urinating',
+    'Auditory Hallucinations',
+    'Visual Hallucinations',
+    'Skin blisters and welts',
+    'Skin rash and yellow skin',
+    'Slurred speech'
+
+
+
+
+
 
 
 
